1d08cd5db6c292b8873559fd549e7bf45cad43e1
javi-domi <javdominguez@icloud.com>
2021-10-07 19:09:17 -0300

    styles: add styles to search results

--- deezer-search/src/components/ArtistList/ArtistList.js
+++ deezer-search/src/components/ArtistList/ArtistList.js
@@ -1,17 +1,22 @@
 import React from "react";
+import styles from "./ArtistList.module.css";
+import "../../App.css";
 
 const ArtistList = ({ artists }) => {
   return (
-    <div>
-      {artists &&
-        artists.map(({ name, id, picture }) => {
-          return (
-            <li key={id}>
-              <p>{name}</p>
-              <img src={picture} alt={name} />
-            </li>
-          );
-        })}
+    <div className={styles.resultContainer}>
+      <h2>Artists:</h2>
+      <div className="listContainer">
+        {artists &&
+          artists.map(({ name, id, picture }) => {
+            return (
+              <li className="itemCard" key={id}>
+                <p className="cardTitle">{name}</p>
+                <img src={picture} alt={name} />
+              </li>
+            );
+          })}
+      </div>
     </div>
   );
 };
