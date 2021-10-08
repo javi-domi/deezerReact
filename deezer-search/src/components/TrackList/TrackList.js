8bf0354818eb5053bb077babdd4c5c6706d7e44e
javi-domi <javdominguez@icloud.com>
2021-10-08 09:34:34 -0300

    feat: show song duration and artist
    Close #3

--- deezer-search/src/components/TrackList/TrackList.js
+++ deezer-search/src/components/TrackList/TrackList.js
@@ -8,29 +8,37 @@ const TrackList = ({ tracks }) => {
       <h2>Songs:</h2>
       <div className={styles.songListContainer}>
         {tracks &&
-          tracks.map(({ title, id, album, link, preview }) => {
-            return (
-              <li className={styles.songCard} key={id}>
-                <div className={styles.songInfo}>
-                  <img
-                    className={styles.songPhoto}
-                    src={album.cover}
-                    alt={title}
-                  />
-                  <p className={styles.songName}>{title}</p>
-                </div>
-                <div className={styles.songLinks}>
-                  <a className={styles.deezerLink} href={link}>
-                    Listen on Deezer
-                  </a>
-                  <span> |</span>
-                  <a className={styles.preview} href={preview}>
-                    Preview
-                  </a>
-                </div>
-              </li>
-            );
-          })}
+          tracks.map(
+            ({ title, id, album, link, preview, duration, artist }) => {
+              return (
+                <li className={styles.songCard} key={id}>
+                  <div className={styles.songInfo}>
+                    <img
+                      className={styles.songPhoto}
+                      src={album.cover}
+                      alt={title}
+                    />
+                    <div>
+                      <p className={styles.songName}>{title}</p>
+                      <p className={styles.songText}>{artist.name}</p>
+                      <p className={styles.songText}>
+                        {(duration / 60).toFixed(2)} minutes
+                      </p>
+                    </div>
+                  </div>
+                  <div className={styles.songLinks}>
+                    <a className={styles.deezerLink} href={link}>
+                      Listen on Deezer
+                    </a>
+                    <span> |</span>
+                    <a className={styles.preview} href={preview}>
+                      Preview
+                    </a>
+                  </div>
+                </li>
+              );
+            }
+          )}
       </div>
     </div>
   );
